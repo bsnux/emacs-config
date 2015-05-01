500d2d4bb1d7c0f86dcf5900d3394e9c4984163f
Arturo Fernandez <arturo@bsnux.com>
2015-05-01 15:20:33 -0700

    JS config. Regular emacs cursor moving keys for evil mode.

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
-;;--------------------------------------------------------
-;;         init.el
-;;
-;;   Configuration file for Emacs running on Mac OS X
-;;--------------------------------------------------------
+;;; Package --- init.el
+;;;  Configuration file for Emacs running on Mac OS X
+;;;
 
 
 ;;; Commentary:
@@ -21,7 +19,7 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
-(setq color-theme-is-global t)
+;(setq color-theme-is-global t)
 
 ;; required by el-get
 (setq recipes
@@ -35,22 +33,33 @@
         yasnippet-snippets
         multiple-cursors
         darcula-theme
+        color-theme-almost-monokai
         fill-column-indicator
         undo-tree
         markdown-mode
         markdown-preview-mode
         evil
+        magit
+        ecb
+        elixir
+        cider
+        haskell-mode
+        color-theme-zenburn
+        solarized-emacs
         autopair))
 (el-get 'sync recipes)
 (el-get 'wait)
 
 (evil-mode 1)
 
-(require 'color-theme)
-(color-theme-initialize)
-(setq color-theme-is-global t)
+;(require 'color-theme)
+;(color-theme-initialize)
+;(stq color-theme-is-global t
 ;;(color-theme-railscasts)
-(require 'darcula-theme)
+;(require 'darcula-theme)
+;(color-theme-almost-monokai)
+(load-theme 'zenburn t)
+;(load-theme 'solarized-dark t)
 
 (require 'fill-column-indicator)
 (setq-default fill-column 80)
@@ -125,8 +134,12 @@
   (setq mouse-sel-mode t)
   )
 
+
+;; scroll window under mouse
+(setq mouse-wheel-follow-mouse 't)
+
 ;; Server: Open files in server with `emacsclient <filename>`
-(server-start)
+;;(server-start)
 
 ;; Highlight current line
 (global-hl-line-mode 1)
@@ -185,6 +198,7 @@
 (global-set-key "\M-j" 'join-line)
 (global-set-key (kbd "<M-up>") 'beginning-of-buffer)
 (global-set-key (kbd "<M-down>") 'end-of-buffer)
+(global-set-key "\M-c" 'copy-to-x-clipboard)
 
 ;; Font type (X Emacs on Mac OS X);;(set-face-attribute 'default nil
 ;;                :family "DejaVu Sans Mono for Powerline" :height 135 :weight 'normal)
@@ -195,3 +209,87 @@
 ;; Let's Emacs X can read env variables from PATH
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" default)))
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq select-active-regions nil)
+(setq mouse-drag-copy-region t)
+(global-set-key [mouse-2] 'mouse-yank-at-click)
+
+(setq *is-a-mac* (eq system-type 'darwin))
+(setq *cygwin* (eq system-type 'cygwin) )
+(setq *linux* (or (eq system-type 'gnu/linux) (eq system-type 'linux)) )
+(defun copy-to-x-clipboard ()
+  (interactive)
+  (if (region-active-p)
+      (progn
+        (cond
+         ((and (display-graphic-p) x-select-enable-clipboard)
+          (x-set-selection 'CLIPBOARD (buffer-substring (region-beginning) (region-end))))
+         (t (shell-command-on-region (region-beginning) (region-end)
+                                     (cond
+                                      (*cygwin* "putclip")
+                                      (*is-a-mac* "pbcopy")
+                                      (*linux* "xsel -ib")))
+            ))
+        (message "Yanked region to clipboard!")
+        (deactivate-mark))
+    (message "No region active; can't yank to clipboard!")))
+
+(defun paste-from-x-clipboard()
+  (interactive)
+  (cond
+   ((and (display-graphic-p) x-select-enable-clipboard)
+    (insert (x-get-selection 'CLIPBOARD)))
+   (t (shell-command
+       (cond
+        (*cygwin* "getclip")
+        (*is-a-mac* "pbpaste")
+        (t "xsel -ob"))
+       1))
+   ))
+
+(defun my/paste-in-minibuffer ()
+  (local-set-key (kbd "M-y") 'paste-from-x-clipboard)
+  )
+
+(add-hook 'minibuffer-setup-hook 'my/paste-in-minibuffer)
+
+;; JavaScript
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq-default js2-basic-offset 2)
+(setq javascript-indent-level 2)
+
+;; Using standard moving keys inside `evil` mode
+(define-key evil-normal-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-insert-state-map "\C-e" 'end-of-line)
+(define-key evil-visual-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-normal-state-map "\C-f" 'evil-forward-char)
+(define-key evil-insert-state-map "\C-f" 'evil-forward-char)
+(define-key evil-insert-state-map "\C-f" 'evil-forward-char)
+(define-key evil-normal-state-map "\C-b" 'evil-backward-char)
+(define-key evil-insert-state-map "\C-b" 'evil-backward-char)
+(define-key evil-visual-state-map "\C-b" 'evil-backward-char)
+(define-key evil-normal-state-map "\C-d" 'evil-delete-char)
+(define-key evil-insert-state-map "\C-d" 'evil-delete-char)
+(define-key evil-visual-state-map "\C-d" 'evil-delete-char)
+(define-key evil-normal-state-map "\C-n" 'evil-next-line)
+(define-key evil-insert-state-map "\C-n" 'evil-next-line)
+(define-key evil-visual-state-map "\C-n" 'evil-next-line)
+(define-key evil-normal-state-map "\C-p" 'evil-previous-line)
+(define-key evil-insert-state-map "\C-p" 'evil-previous-line)
+(define-key evil-visual-state-map "\C-p" 'evil-previous-line)
+;;; init.el ends here
